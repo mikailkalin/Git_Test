b0cb10b4e7a36dfc779562dfbbcba43aad49e288
mikailkalin <67229483+mikailkalin@users.noreply.github.com>
2020-06-21 17:05:15 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,8 @@
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("Git den Merhaba");
+        System.out.println("Git den Merhaba");        System.out.println("Git den Merhaba");
+        
+        
     }
 }
